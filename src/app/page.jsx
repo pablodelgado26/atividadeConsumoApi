17c1be6586466a5d3a2780177a3a58884d27d8c3
pablodelgado26 <158165561+pablodelgado26@users.noreply.github.com>
2025-04-15 10:28:50 -0300

    feat: criação de componente hpList

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import HPList from "../components/hpList";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <main className={styles.main}>
+        <HPList />
+      </main>
 
+      <footer className={styles.footer}>
+        <p>Desenvolvido durante o curso de Desenvolvimento de Sistemas</p>
+      </footer>
     </div>
   );
 }
